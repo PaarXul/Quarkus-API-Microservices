4a855b5ef22ed7fa5f445fcf6ae9bc6265ee2e69
PaarXul <gian.parra96@gmail.com>
2025-01-18 00:21:34 -0300

    Se Inicializa el proyecto junto con los proyectos creados

---
+++ category-service/src/test/java/org/acme/CategoryResourceIT.java
@@ -0,0 +1,8 @@
+package org.acme;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class CategoryResourceIT extends CategoryResourceTest {
+    // Execute the same tests but in packaged mode.
+}
